72d1a458d291c7c489983f9aac00949d4181dbba
joaomarcos9741 <joaommarcosc@gmail.com>
2023-08-11 08:22:46 -0300

    11 agosto

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,18 @@
+import styles, { createGlobalStyle } from "styled-components"
+
+export const GlobalStyle = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        outline: 0;
+        border: 0;
+        box-sizing: border-box;
+    }
+`
+
+export const colors = {
+    primary: "#43E8DE",
+    secondary: "#000000",
+    third: "#F0F0F0",
+    while: "#ffffff"
+}
