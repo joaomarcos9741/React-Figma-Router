57fa1c907aa6d19ee6836b20ba040a7bfad45fe9
joaomarcos9741 <joaommarcosc@gmail.com>
2023-08-23 14:09:30 -0300

    23 agosto

--- src/pages/Biblioteca/index.tsx
+++ src/pages/Biblioteca/index.tsx
@@ -1,11 +1,22 @@
-import styled from 'styled-components'
 
-export const Biblioteca = styled.section`
-  display: flex;
-  max-width: 1090px;
-  margin: 0 auto;
-  img {
-    width: 50%;
-  }
-`
+import * as S from "./styles"
+import  Livro1 from "../../assets/Livro1.png"
+import Livro2 from "../../assets/Livro2.png"
+import Livro3 from "../../assets/Livro3.png"
+import  Livro4  from "../../assets/Livro.png"
+
+
+export function Biblioteca(){
+  return(
+    <>
+          <img src={Livro1}/>
+          <img src={Livro2}/>
+          <img src={Livro3}/>
+          <img src={Livro4}/>
+
+    </>
+        
+
+  )
+}
 
